0746ace617ebba4daac0296a346deb2c38cc6d4c
friza diga <friza diga>
2017-09-25 17:40:56 +0700

    add datalist

--- src/renderer/components/index.vue
+++ src/renderer/components/index.vue
@@ -6,7 +6,17 @@
   <iframe id="content" :src="contentSRC" frameborder="0"></iframe>
 
   <div id="footer" class="w3-bottom w3-blue-grey">
-    <input type="text" id="input-src" v-model="contentSRC" class="w3-blue-grey">
+    <input id="input-src" v-model="contentSRC" @change="changeView" class="w3-blue-grey" list="urls">
+    <datalist id="urls">
+      <option value="http://hiuwave.com">hiuwave</option>
+      <option value="http://localhost:1700">f7-vue</option>
+      <option value="https://framework7.io/kitchen-sink-material/">f7 Kitchen Sink Material</option>
+    </datalist>
+    <!-- <select name="" id="">
+      <option value="1"></option>
+      <option value="2"></option>
+      <option value="3"></option>
+    </select> -->
     <span id="title" class="w3-right"><b>hiuME <i class="fa fa-arrows-alt"></i></b></span>
   </div>
 
@@ -16,7 +26,8 @@
 </template>
 
 <script>
-
+let content = document.getElementById('content');
+let inputSRC = document.getElementById('input-src');
 module.exports = {
   name:'index',
   components:{},
@@ -25,7 +36,7 @@ module.exports = {
   }),
   methods:{
     changeView: () => {
-      content.src = input.value;
+      content.src = inputSRC.value;
     }
   }
 }
@@ -44,19 +55,21 @@ module.exports = {
       /*height: 100vh;*/
     }
     #footer{
+      /*z-index: 100;*/
       margin-top: 0px;
       padding: 1px 2px 1px 2px;
       /*height: 30px;*/
       /*background-color: rgb(11, 61, 73)!important;*/
       /*color: white;*/
       bottom:0;
-      position: fixed;
+      position: ;
     }
     #title{
       -webkit-app-region: drag;
       padding: 0px 4px 0 0  ;
     }
     #input-src{
+      /*z-index: 101;*/
       -webkit-app-region: no-drag;
       height: 20px;
       width: 70%;
